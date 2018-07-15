2f3735597fbe22cb8ef8d1037ee3a434fec4f2d8
PC <p.cz@outlook.com>
2018-07-15 21:51:56 +0200

    Zadanie 4 dodatkowe (KOT)

---
+++ src/com/example/kot/NapisKot.java
@@ -0,0 +1,14 @@
+package com.example.kot;
+
+public class NapisKot {
+
+    public static void main(String[] args) {
+
+        char a = 75;
+        char b = 79;
+        char c = 84;
+        System.out.print(a);
+        System.out.print(b);
+        System.out.print(c);
+    }
+}
