f8752e0f14d84d9607ba7661e03ae1a3863518b8
PC <p.cz@outlook.com>
2018-07-19 16:22:23 +0200

    Zadanie 4.2 dokończone

--- src/com/example/zadanie4_2/LineCompare.java
+++ src/com/example/zadanie4_2/LineCompare.java
@@ -1,7 +1,15 @@
 package com.example.zadanie4_2;
 
 public class LineCompare {
-
+    // odwołanie do klasy reprezentowanej przez Line
+    public Line longerLine(Line l1, Line l2){
+        int l1lenght = l1.getP2().getX()-l1.getP1().getX(); // ogarnąć kurwa
+        int l2lenght = 5;
+        if(l1lenght>l2lenght)
+            return l1;
+        else
+            return l2;
+    }
 
 }
 
--- src/com/example/zadanie4_2/LineTest.java
+++ src/com/example/zadanie4_2/LineTest.java
@@ -4,29 +4,39 @@ import java.util.Locale;
 import java.util.Scanner;
 
 public class LineTest {
-//zadanie nieukońcozne
+
     public static void main(String[] args) {
 
         Scanner input = new Scanner(System.in);
         input.useLocale(Locale.US);
-        System.out.println("Podaj początek odcinka 1: ");
+        System.out.println("Podaj początek odcinka 1 (cm): ");
         int odcinek1punktA = input.nextInt();
-        System.out.println("Podaj koniec odcinka 1: ");
+        System.out.println("Podaj koniec odcinka 1 (cm):");
         int odcinek1punktB = input.nextInt();
-        System.out.println("Podane punkty: " + odcinek1punktA +" "+ odcinek1punktB);
 
-       Point odcinek1 = new Point(odcinek1punktA,odcinek1punktB);
+        Point odcinek1 = new Point(odcinek1punktA, odcinek1punktB);
 
-        System.out.println("Podaj początek odcinka 2: ");
+        System.out.println("Podaj początek odcinka 2 (cm): ");
         int odcinek2punktA = input.nextInt();
-        System.out.println("Podaj koniec odcinka 2: ");
+        System.out.println("Podaj koniec odcinka 2 (cm):");
         int odcinek2punktB = input.nextInt();
-        System.out.println("Podane punkty: " + odcinek2punktA +" "+ odcinek2punktB);
 
         Point odcinek2 = new Point(odcinek2punktA, odcinek2punktB);
 
-        System.out.println("Odcinek pierwszy: " + odcinek1.getX() + " " + odcinek1.getY());
-        System.out.println("Odcinek drugi: " + odcinek2.getX() + " " + odcinek2.getY());
+        System.out.println("Początek odcinka 1: " + odcinek1.getX() + ", Koniec odcinka 1: " + odcinek1.getY());
+        int dlugosc1 = odcinek1.getY() - odcinek1.getX();
+        System.out.println("Całkowita długość odcinka 1 = " + dlugosc1 + " cm");
+        System.out.println("Początek odcinka 2: " + odcinek2.getX() + ", Koniec odcinka 2: " + odcinek2.getY());
+        int dlugosc2 = odcinek2.getY() - odcinek2.getX();
+        System.out.println("Całkowita długość odcinka 2 = " + dlugosc2 + " cm");
+
+        if (dlugosc1 > dlugosc2) {
+            System.out.println("Odcinek 1 jest dłuższy od odcinka 2");
+        } else if (dlugosc1 < dlugosc2) {
+            System.out.println("Odcinek 2 jest dłuższy od odcinka 1");
+        } else {
+            System.out.println("Odcinki są sobie równe");
+        }
 
     }
 }
